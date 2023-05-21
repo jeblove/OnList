8dd7433792451f94325b67f783e42d5b8c359e51
jeblove <249972068@qq.com>
2023-05-22 01:30:04 +0800

    上传文件api；删除文件api；上传到相同位置，不进行link记录；优化结构

--- src/main/java/com/jeblove/onList/service/FileService.java
+++ src/main/java/com/jeblove/onList/service/FileService.java
@@ -1,12 +1,17 @@
 package com.jeblove.onList.service;
 
+import com.jeblove.onList.common.FileCheckHashUtil;
 import com.jeblove.onList.common.Result;
+import com.jeblove.onList.entity.FileLink;
+import com.jeblove.onList.entity.User;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSDownloadStream;
 import com.mongodb.client.gridfs.model.GridFSFile;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -16,11 +21,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -31,15 +38,24 @@ import java.util.List;
  */
 @Service
 public class FileService {
+    @Value("${fileLink.hashType}")
+    private String hashType;
+    @Value("${fileLink.fileLinkPath}")
+    private String fileLinkPath;
 
     @Autowired
     private MongoTemplate mongoTemplate;
-
     @Autowired
     private GridFsTemplate gridFsTemplate;
-
     @Autowired
     private GridFSBucket gridFSBucket;
+    @Autowired
+    @Lazy
+    private FileLinkService fileLinkService;
+    @Autowired
+    private PathService pathService;
+    @Autowired
+    private UserService userService;
 
     /**
      * 获取文件信息
@@ -94,7 +110,7 @@ public class FileService {
     }
 
     /**
-     * 删除文件
+     * 删除fs.files的文件
      * @param id 文件id
      * @return 成功code：200，失败code：500
      */
@@ -148,6 +164,84 @@ public class FileService {
                     .body(streamingResponseBody);
         }
         return responseEntity;
+    }
+
+    /**
+     * 上传文件
+     * @param uploadFile 文件
+     * @param userId 用户id
+     * @param pathList 上传到目录的列表
+     * @return data.fileLinkId 文件链接id
+     * @throws Exception
+     */
+    public Result uploadFile(MultipartFile uploadFile, String userId, @RequestParam List<String> pathList) throws Exception {
+        User user = userService.getUser(userId);
+        String username = user.getUsername();
+
+        System.out.println(uploadFile.getOriginalFilename()+" 文件大小："+uploadFile.getSize());
+        // .getName():uploadFile  getOriginalFilename():文件名.xxx
+        String fileLinkId;
+        String msg;
+
+        // 检测md5
+        String hashCode = FileCheckHashUtil.getHashCode(uploadFile);
+        // 已存在检测
+        if(!fileLinkService.fileLinkExists(hashType, hashCode)){
+            // False: 文件不存在，则上传文件
+            Result fileResult = storeFile(uploadFile, uploadFile.getOriginalFilename());
+            String fileId = (String) fileResult.getData();
+
+            System.out.println(fileLinkPath);
+            // 添加FileLInk记录
+            FileLink fileLink = fileLinkService.insertFileLink(fileId, hashType, hashCode, fileLinkPath, username);
+            fileLinkId = fileLink.getId();
+            msg = "文件不存在";
+        }else{
+            // True: 文件存在，查询链接，返回linkId
+            FileLink fileLink = fileLinkService.findFileLinkByHashCode(hashCode);
+            fileLinkId = fileLink.getId();
+            // 添加链接数和用户名
+            fileLinkService.appendFileLink(hashCode, username);
+            msg = "文件已存在";
+        }
+        HashMap<String, String> map = new HashMap<>();
+        map.put("fileLinkId",fileLinkId);
+        map.put("msg",msg);
+
+        // 添加fileLinkId到指定目录
+        long count = pathService.addFileToPath(user.getPathId(), uploadFile.getOriginalFilename(), fileLinkId, pathList);
+        map.put("count", String.valueOf(count));
+
+        // 上传到相同位置，不进行链接+1
+        if(fileLinkId != null && count==0){
+            fileLinkService.deleteFileLinkUpdate(fileLinkId, user.getUsername());
+        }
+
+        return Result.success(map);
+    }
+
+    /**
+     * 删除文件
+     * @param userId 用户id
+     * @param filename 文件名
+     * @param pathList 所在目录（不包含）
+     * @return code 200:成功 500失败
+     */
+    public Result deleteFile(String userId, String filename,@RequestParam List<String> pathList){
+        User user = userService.getUser(userId);
+
+        // 获取fileLinkId并从path中删除该文件
+        String fileLinkId = pathService.deleteFile(user.getPathId(), filename, pathList);
+        System.out.println(fileLinkId);
+        if(fileLinkId==null){
+            return Result.error(500, "文件或目录不正确");
+        }
+        // 更新链接文件
+        Result result = fileLinkService.deleteFileLinkUpdate(fileLinkId, user.getUsername());
+        if(result.getCode() != 200){
+            return Result.error(500, "删除链接文件失败");
+        }
+        return result;
     }
 
 }
